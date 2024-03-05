83ebc1ddd884d16d42d2c11d63689fe4f4535e0c
euluis-dev <fluisf00@gmail.com>
2024-03-05 01:48:11 -0300

    Add functionality for CEP lookup using fetch() and focusout event listener.
    Utilizes the ViaCEP API to fetch information for the CEP entered by the user. Implements promise handling with fetch().then(), converting the response to JSON and logging it to the console.
    
    Refactors the code to adhere to strict mode ('use strict').

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const cep = document.querySelector("#cep");
+
+const pesquisarCep = () => {
+    const inputCep = cep.value;
+    const url = `http://viacep.com.br/ws/${inputCep}/json/`;
+    fetch(url)
+        .then(response => {  //return promess assync
+            return console.log(response.json());
+        })  
+
+};
+
+cep.addEventListener('focusout', pesquisarCep); 
