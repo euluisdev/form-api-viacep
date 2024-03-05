c235e2a9a996f1247b5b95fabde9d25a99423df2
euluis-dev <fluisf00@gmail.com>
2024-03-05 10:16:46 -0300

    the completForm function was created taking the data from dadosAll

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,12 +1,17 @@
 'use strict';
 
 const cep = document.querySelector('#cep');
-const rua = document.querySelector('#endereco')
+const endereco = document.querySelector('#endereco');
+
+const completForm = (dadosAll) => {
+    endereco.value = dadosAll.logradouro;
+    
+};
 
 const pesquisarCep = async () => {
     const inputCep = cep.value;
     const url = `http://viacep.com.br/ws/${inputCep}/json/`;
-    const dados = await fatch(url);
+    const dados = await fetch(url);
     const dadosAll = await dados.json();
     completForm(dadosAll);
 };
