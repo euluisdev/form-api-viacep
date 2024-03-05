774c2ea5295e383e9252539ec3780081c5df3dad
euluis-dev <fluisf00@gmail.com>
2024-03-05 14:10:41 -0300

    'ifelse' was criated to handle errors in the pesquisarCep function and thecompletForm function was created to handle the response data and display it on the screen

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -6,20 +6,24 @@ const bairro = document.querySelector('#bairro');
 const cidade = document.querySelector('#cidade');
 const estado = document.querySelector('#estado');
 
-
-const completForm = (dadosAll) => {
-    endereco.value = dadosAll.logradouro;
-    bairro.value = dadosAll.bairro;
-    cidade.value = dadosAll.localidade;
-    estado.value = dadosAll.uf;
+const completForm = (dados) => {
+    endereco.value = dados.logradouro;
+    bairro.value = dados.bairro;
+    cidade.value = dados.localidade;
+    estado.value = dados.uf;
 };
 
 const pesquisarCep = async () => {
     const inputCep = cep.value;
     const url = `http://viacep.com.br/ws/${inputCep}/json/`;
-    const dados = await fetch(url);
-    const dadosAll = await dados.json();
-    completForm(dadosAll);
+    const response = await fetch(url);
+    const dados = await response.json();
+    
+    if(dados.hasOwnProperty('erro')) {
+        endereco.value = 'CEP não encontrado!';
+    } else {
+       completForm(dados); 
+    }
 };
 
 cep.addEventListener('focusout', pesquisarCep); 
