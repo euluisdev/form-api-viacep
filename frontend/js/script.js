8a2174952fcee6b48834f66ea0ad47f924e3f900
euluis-dev <fluisf00@gmail.com>
2024-03-06 18:30:55 -0300

    object-oriented programming was applied to the code

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,4 +1,4 @@
-'use strict';
+/* 'use strict';
 
 const cep = document.querySelector('#cep');
 const endereco = document.querySelector('#endereco');
@@ -46,4 +46,62 @@ const pesquisarCep = async () => { //this searche the zip code and fill out the 
     
 };
 
-cep.addEventListener('focusout', pesquisarCep); 
+cep.addEventListener('focusout', pesquisarCep);  */
+
+
+
+                    // #POO
+'use strict';  
+
+class FormCep {
+    constructor() {
+        this.cep = document.querySelector('#cep');
+        this.endereco = document.querySelector('#endereco');
+        this.bairro = document.querySelector('#bairro');
+        this.cidade = document.querySelector('#cidade');
+        this.estado = document.querySelector('#estado');
+
+        this.cep.addEventListener('focusout', this.pesquisarCep);
+    }
+
+    limparForm = (dados) => {
+        this.endereco.value = '';
+        this.bairro.value = '';
+        this.cidade.value = '';
+        this.estado.value = '';
+    }
+
+    completForm = (dados) => {
+        this.endereco.value = dados.logradouro;
+        this.bairro.value = dados.bairro;
+        this.cidade.value = dados.localidade;
+        this.estado.value = dados.uf;
+    }
+
+    eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+    validarCep = (inputCep) => inputCep.length == 8 && this.eNumero(inputCep);
+
+    pesquisarCep = async () => {
+        this.limparForm();
+
+        const inputCep = this.cep.value;
+        const url = `http://viacep.com.br/ws/${inputCep}/json/`;
+
+        if (this.validarCep(inputCep)) {
+            const response = await fetch(url);
+            const dados = await response.json();
+
+            if (dados.hasOwnProperty('erro')) {
+                this.endereco.value = `CEP não encontrado!`;
+            } else {
+                this.completForm(dados);
+            }
+        } else {
+            this.endereco.value = `CEP incorreto! Digite um CEP válido.`
+        }
+
+    }
+};
+
+const form = new FormCep();
