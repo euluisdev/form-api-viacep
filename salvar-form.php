393e74455b67ed4357c95795690fb8f4e399d75b
euluis-dev <fluisf00@gmail.com>
2024-03-10 17:44:19 -0300

    variables were created in PHP with the values ​​coming from the form

--- salvar-form.php
+++ salvar-form.php
@@ -1,2 +1,14 @@
 <?php 
-    
+        switch ($_POST["acao"]) {
+            case 'cadastrar':
+                $nome = filter_var($_POST['nome'], FILTER_SANITIZE_STRING);
+                $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+                $cep = filter_var($_POST['cep'], FILTER_SANITIZE_NUMBER_INT);
+                $endereco = filter_var($_POST['endereco'], FILTER_SANITIZE_STRING);
+                $numero = filter_var($_POST['numero'], FILTER_SANITIZE_STRING);
+                $bairro = filter_var($_POST['bairro'], FILTER_SANITIZE_STRING);
+                $cidade = filter_var($_POST['cidade'], FILTER_SANITIZE_STRING);
+                $estado = filter_var($_POST['estado'], FILTER_SANITIZE_STRING);
+                
+                break;
+            }
